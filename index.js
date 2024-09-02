37f7fd7aa094b598e8c07b0f2fe6ae1e7b0a2a7b
Dharanisri314 <dharanisria@2024>
2024-09-02 17:11:06 +0530

    Initial commit - portfolio website

---
+++ index.js
@@ -0,0 +1,17 @@
+//  a = 5;
+//  b = 6;
+//  c=a;
+//  a=b;
+//  b=c;
+// console.log(a,b);
+// a = 100;
+// b = 6;
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// console.log(a,b)
+a = 25;
+b = 60;
+console.log(a,b);
+ 
+
