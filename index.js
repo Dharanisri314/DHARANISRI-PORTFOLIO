7662cdac1d7f8c5442620c887859ddfc28db867b
Dharanisri314 <dharanisria@2024>
2024-09-05 14:06:48 +0530

    modified file 1

--- index.js
+++ index.js
@@ -10,8 +10,110 @@
 // b=a-b;
 // a=a-b;
 // console.log(a,b)
-a = 25;
-b = 60;
-console.log(a,b);
+// a = 25;
+// b = 60;
+// console.log(a,b);
+//  mark =85;
+//  if(mark>=60){
+//     console.log("first class");
+//  }else if(mark<=45){
+//     console.log("second class");
+//  }else{
+//     console.log("thired class"); 
+//  }
+//  let marks = 0;
+//  let grade;
+//  if (marks>60){
+//     console.log("first class");
+//     grade = "A"
+//  }else if(marks>45){
+//     console.log("second class");
+//     grade = "B"
+//  }else{
+//     console.log("fail")
+//     grade ="no grade"
+//  }
+
+
+
+//  let day = 4;
+//  switch (day) {
+//      case 1:
+//          console.log("Today is Monday");
+//          break;
+//      case 2:
+//          console.log("Today is Tuesday");
+//          break;
+//      case 3:
+//          console.log("Today is Wednesday");
+//          break;
+//      case 4:
+//          console.log("Today is Thursday");
+//          break;
+//      case 5:
+//          console.log("Today is Friday");
+//          break;
+//      case 6:
+//          console.log("Today is Saturday");
+//          break;
+//      case 7:
+//          console.log("Today is Sunday");
+//          break;
+//      default:
+//          console.log("Invalid day");
+//          break;
+//  }
+let vowel ="b"
+switch(vowel){
+    case "a":
+    console.log("vowel");
+    break;
+    case "e":
+        console.log("vowel");
+        break;
+     case "i":
+        console.log("vowel");
+        break;
+      case "o":
+        console.log("vowel"); 
+        break;
+      case "u":
+        console.log("vowel");
+        break;
+        default:
+            console.log("charactor is not vowel");   
+            break;   
+// }
+// let age = 5;
+// if(age>=5){
+//     console.log("free");
+// }else if(age<=12){
+//     console.log("10");
+// }else{
+//     console.log("15");
+// }
+a = 12;
+b = 16;
+if(a<b){
+    console.log("a is a smallest number");
+}else if(b>a){
+    console.log("a is a smallest number");
+}else{
+    console.log("a is a smallest number" );
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
