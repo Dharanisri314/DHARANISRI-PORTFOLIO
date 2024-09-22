637ce71c8e29449a7a8e957bc79f61176fb8d07b
Dharanisri314 <dharanisria@2024>
2024-09-22 17:06:29 +0530

    modified file 2

--- index.js
+++ index.js
@@ -115,7 +115,7 @@
 
 // let lap = 1;
 // let count = 10;
-// while(Lap<=count){
+// while(lap<=count){
 //     console.log("lap"+lap);
 //     lap++;
 // }
@@ -135,26 +135,26 @@
 
 // 3 question:
 
-directions(2)
-function directions(n){
-    switch(n){
-        case 1:
-            console.log("North");
-            break;
-        case 2:
-            console.log("South");
-             break;
-        case 3:
-            console.log("East");
-            break;
-        case 4:
-            console.log("West");
-            break;
-        default:
-            console.log("invalide");
-     }
+// directions(2)
+// function directions(n){
+//     switch(n){
+//         case 1:
+//             console.log("North");
+//             break;
+//         case 2:
+//             console.log("South");
+//              break;
+//         case 3:
+//             console.log("East");
+//             break;
+//         case 4:
+//             console.log("West");
+//             break;
+//         default:
+//             console.log("invalide");
+//      }
     
-}
+// }
 
 // //  4th question:
 
@@ -228,18 +228,372 @@ function directions(n){
 // fruits[1]=fruits[4];
 // console.log(fruits);//[ 'Apple', 'Mango', 'Graps', 'kiwi', 'Mango' ]
 
-function greet(name) {
-    console.log(`Hello, ${name}`);
-}
+// function greet(name) {
+//     console.log(`Hello, ${name}`);
+// }
 
-function greetGoodAfternoon(name){
-    console.log(`Good afternoon,${name}`)
-}
+// function greetGoodAfternoon(name){
+//     console.log(`Good afternoon,${name}`)
+// }
 
-function processUserInput(callback) {
-    const name = console.log("Please enter your name:");
-    callback(name);
-}
+// 1 question:
 
-processUserInput(greet)
+// let numbers = [7,5,3,1,10];
+// for(let i= 0;i<numbers.length;i++){
+//     console.log(numbers[i]);
+// }
+
+// 2 question:
+
+// color = ["blue","pink","green","white"]
+//     console.log(color[1]);
+//     console.log(color[3]);
+
+// 3 question:    
+
+// let name =["Ramya","Hari","Princy"];
+// name[1] = "Dharanisri";
+// console.log(name);
+
+// 4 question:
+
+// let num=[1,2,3,4,5,6,7];
+// console.log(num.length);
+
+// 5 question:
+
+// let grade = ["I","Like","Myself"];
+// grade.forEach(function(item){
+//     console.log(item);
+// });
+
+
+// let soldiers =[120,300,250,180,150];
+// let maxindex = soldiers.indexOf(Math.max(soldiers));
+// console.log("the hight value is" &{name});
+
+// let n=1;
+// while(n<=10){
+//     console.log("The robot completes the lap :"+ n);
+//     n++;
+// }
+// console.log("Robot completes all");
+//
+
+// 2 question:
+
+// let m=100;
+// while(m>20){
+//     console.log("Battery level:"+m+"%");
+//     m-=5;
+// }
+// console.log("Battery is low");
+
+// // 3.An airplane altitude currently is 1000ft and its altitude reduces each second by 10ft. Write a program which will print “Flight arrived” when the airplane altitude reaches 0ft .
+// let i=1000;
+// while(i>0){
+//     console.log("Flight is travelling:"+" " + i+"ft");
+//     i-=10;
+// }
+// console.log("Flight has arrived");
+// // 4.Write a program where an airplane climbs to a specific altitude(say 1000ft) over 10 intervals. Use a for loop to display the altitude after each interval.
+// for(let i=100;i<=1000;i+=100){
+//   console.log("Interval"+" "+i+"ft");
+// }
+// console.log("1000ft in 10th interval");
+// // 5.Based on the flight’s destination, direct the airplane to a specific runway. Use a switch statement to assign the runway depending on the destination entered by the user.
+// let Mydestination="Tokyo";
+// switch(Mydestination){
+//     case 'Chicago':
+//         console.log("Chicago")
+//         break;
+//     case 'Mexico':
+//         console.log("Mexico");
+//         break;
+//     case 'Tokyo':
+//         console.log("Tokyo");
+//         break;
+//     default:
+//         console.log("Wrong destination");
+//         break;
+// }
+// // 3rd one therla d unku therinja snd panu
+// let train_departures=[10,15,7,20,5];
+// let temps=train_departures[0];
+// for(let i=0;i<train_departures.length;i++){
+//        if(train_departures[i]<temps){
+//         temps=train_departures[i];
+//        }
+// }
+// console.log(temp);
+// let seats = [1,0,1,1,0,0,0,0,1];
+// let lense= seats.length;
+// let tem=seats[0];
+// let con=0;
+// for(let i=0;i<lense;i++)
+//   if(seats[i]===0){
+//     count++;
+// }
+// console.log(`number of seats available are ${count}`);
+// let soldiers = [120, 300, 250, 180, 150];
+// let tems=soldiers[0];
+// for(let i=0; i<soldiers.length;i++){
+//     if(temp<soldiers[i]){
+//         temp=soldiers[i];
+//     }
+// }
+// console.log(temp);
+// let populations = [50, 60, 70, 80, 90];
+// let sum=0;
+// let avg=0;
+// let lens=populations.length
+// for(let i=0;i<len;i++){
+//     sum+=populations[i];
+//      avg=sum/len;
+// }
+// console.log(avg);
+// let water_levels = [30, 50, 20, 40, 60];
+// let liq= water_levels[0];
+// let low=water_levels.length;
+// for(let i=0;i<len;i++){
+//     if(temp>water_levels[i]){
+//         temp=water_levels[i];
+//     }
+// }
+// console.log(temp);
+// let days_until_festival = [30, 15, 45, 10, 25];
+// let grow=days_until_festival[0];
+// let lower=days_until_festival.length;
+// for(let i=0;i<len;i++){
+//     if (temp>days_until_festival[i]){
+//         temp=days_until_festival[i];
+//     }
+// }
+// console.log(temp);
+// let landing_times = [10, 15, 12, 18, 10];
+// let len=landing_times.length;
+// let temp=landing_times[0];
+// let count=0;
+// for(let i=0; i<len;i++){
+//     if(temp===landing_times[i]){
+//         count++;
+//         if(count>1){
+//             console.log("more than one");
+//         }
+//     }
+// }
+// let  temperatures = [75, 80, 72, 85, 90];
+// let len= temperatures.length;
+// let temp=temperatures[0];
+// for(let i=0;i<len;i++){
+//     if(temperatures[i]>80){
+//         console.log(temperatures[i]);
+//     }
+// }
+// let students_per_route = [45, 55, 30, 60, 40];
+// let len=students_per_route.length;
+// let temp=students_per_route[0];
+// let busNeeded=0;
+// for(let i=0;i<len;i++){
+//     if(students_per_route[i]>50){
+//         busNeeded++;
+//     }
+// }
+// console.log(busNeeded);
+
+
+// 1 QUESTION:
+
+// let fruits=["apple","orange","mango","cherry","banana"];
+// console.log(fruits.length);
+
+// // 2 QUESTION:
+// let color=["pink","blue","white","orange"];
+// console.log(color.indexOf("green"));
+
+// // 3 QUESTION:
+
+// let animal=["king","dog","cat"];
+// animal.push("cheeta");
+// console.log(animal);
+
+// // 4 QUESTION:
+
+// let car=["BMW","thar","audi","suzuki"];
+// car.pop("suzuki");
+// console.log(car);
+
+// // 5 QUESTION:
+
+// let gadgets=["laptop","keyboard","monitor"];
+// gadgets.shift("laptop");
+// console.log(gadgets);
+
+// // 6 QUESTION:
+
+// let cities=["trichy","chennai"];
+// cities.unshift("uk");
+// console.log(cities);
+
+// // 7 QUESTION:
+
+// let pl=["html","css","js"];
+// let statement = pl.join(",");
+// console.log(statement);
+
+// 8 QUESTION:
+
+// let str = "dog,cat,bird"
+// let more = str.split(",");
+// console.log(more);
+
+
+// 9 QUESTION:
+
+// let countries=["india","asia","africa"];
+// countries.push("south africa");
+// console.log(countries);
+
+// let lastcountries=countries.pop();
+// console.log(lastcountries);
+// console.log(countries);
+
+
+// 10 QUESTION:
+
+// let items=["pen","pencil","phone"];
+// items.unshift("monitor");
+// console.log(items);
+// items.shift();
+// console.log(items);
+
+// 11 QUESTION:
+
+// let numbers=[1,2,3,4,5,6];
+// console.log(numbers[numbers.length-1]);
+
+
+
+// vinit gore
+
+// 1 QUESTION:
+
+// let sum = 0;
+//  let i = 0;
+// while(sum==i){
+//     sum = sum+i;
+//     i++;
+//     console.log(sum);
+// }
+
+// Object:
+
+// let students1={
+//     name:"Ramya",
+//     age :26,
+//     phone_no:"1234567890"
+// }
+// console.log(students1)
+
+// let students2={
+//     name:"princy",
+//     age :17,
+//     phone_no:"2345678901"
+// }
+// console.log(students2.name)
+
+//  let students=[
+//     {
+//         name:"paintamil",
+//         age :16,
+//         phone_no:"2767762928"
+//     },
+//     {
+//         name:"kani",
+//         age:17,
+//         phone_no:"62736429349"
+//     },
+//     {
+//         name:"vinit",
+//         age:18,
+//         pnone_no:"6538642737"
+//     }
+// ]
+// console.log(students[0].name)//5th one answer
+
+// let a =[1,2,3]
+// if(0<a.indexOf(2)){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// let num=22;
+// if(num%11==0){
+//     console.log("Divisiable");
+// }else{
+//     console.log("not divisiable");
+// }
+
+// let laptop={
+//     model: "monitor",
+//     ram:"8gb",
+//     osversion:35,
+//     isGPUAvailable:"true"
+// }
+// console.log(laptop);
+
+
+// Today:
+
+// let daily=[30,40,50]
+// let sum=0;
+// for(i=0;i<daily.length;i++)
+// {
+//   sum+=daily[i];
+// }
+// console.log(sum);
+
+// let sum=0;
+// let array=[30,40,50,60,70]
+// for(i=0;i<array.length;i++){
+//     sum+=array[i]
+//     avg=sum+array.length
+// }
+// console.log(avg);
+
+// let age=[18,20,10,17]
+// for(i=0;i<=age.length;i++){
+//     if(age[i]>=18){
+//         console.log(age[i])
+//      }
+// }
+
+// let even=[2,4,6,7,8]
+//   let cound=0;
+// for(i=0;i<even.length;i++){
+//     if(even[i]%2==0){
+//      cound++;
+//     }
+// }
+// console.log(cound)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
