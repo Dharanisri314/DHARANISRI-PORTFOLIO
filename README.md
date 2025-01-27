a9d2a9e4fe41365a2d137285c204dec487da0655
Dharanisri314 <dharanisria2@gmail.com>
2025-01-27 11:17:52 +0530

    add certificate

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Hi All Iam Dharanisri.
 
 My portfolio is inspired by the following design:
-![design]()
+![design]
+<a href="https://vaibhavsinghvi.netlify.app/)">
--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+// let cap=["A","B","C","a","b","c"];
+// for(i=0;i>cap.length;i++){
+//     if(cap==0){
+//         console.log(cap);
+//     }
+// }
 
+// let a=10;
+// let b=80;
+// for(i=a;i<=b;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+const whatsappButton = document.querySelector('.whatsapp-button');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    whatsappButton.style.display = 'block';
+  } else {
+    whatsappButton.style.display = 'none';
+  }
+});
 
 
 
